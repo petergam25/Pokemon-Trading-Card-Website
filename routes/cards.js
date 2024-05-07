ee23b926b0f19819a0be82481d204659adc3577b
petergam10 <156098633+petergam10@users.noreply.github.com>
2024-05-07 16:35:08 +0100

    User Collection Working

--- routes/cards.js
+++ routes/cards.js
@@ -16,6 +16,8 @@ router.get('/', (req, res) => {
   const query = req.query.query || ''; // Default query to blank
   const sort = req.query.sort || 'name ASC'; // Default sort to Series_ID
 
+  let cardIDs = []; // Initialize cardIDs as an empty array
+
   // Query to count total records without applying limit and offset
   const countSQL = `SELECT COUNT(*) AS totalCount FROM cards WHERE name LIKE ?`;
   console.log(countSQL);
@@ -31,47 +33,83 @@ router.get('/', (req, res) => {
     console.log('Total Records:', totalRecords);
     console.log('Total Pages:', totalPages);
 
-    let UserCollectionIdSQL = `SELECT id FROM collection WHERE user_id = ? AND collection_type_ID = 1`;
-    connection.query(UserCollectionIdSQL, [userId], (err, UserCollectionId) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send('Internal Server Error');
-      }
+    if (req.session.user) {
 
-      let UserCollectionCardsSQL = `SELECT * FROM collections_cards WHERE collection_ID = ?`
-      connection.query(UserCollectionCardsSQL, [UserCollectionId], (err, UserCollectionCards) => {
+      let UserCollectionIdSQL = `SELECT id FROM collection WHERE user_id = ? AND collection_type_ID = 1`;
+      connection.query(UserCollectionIdSQL, [userId], (err, UserCollectionId) => {
         if (err) {
           console.error(err);
           return res.status(500).send('Internal Server Error');
         }
 
-        console.log(UserCollectionCards)
+        console.log('User ID: ', userId)
+        console.log('Collection ID: ', UserCollectionId[0].id);
 
-        // Query to fetch paginated data with limit and offset
-        let cardsSQL = `SELECT * FROM cards WHERE name LIKE ? ORDER BY ${sort} LIMIT ? OFFSET ?`;
-        console.log(cardsSQL);
-        connection.query(cardsSQL, [`%${query}%`, limit, offset], (err, result) => {
+        let UserCollectionCardsSQL = `SELECT * FROM collections_cards WHERE collection_ID = ?`
+        connection.query(UserCollectionCardsSQL, [UserCollectionId[0].id], (err, UserCollectionCards) => {
           if (err) {
             console.error(err);
             return res.status(500).send('Internal Server Error');
           }
 
-          // Render your page with the paginated data and total pages
-          res.render('cards/cards', {
-            user: req.session.user,
-            displayName: req.session.displayName,
-            cardsList: result,
-            limit,
-            currentQuery: query,
-            currentSort: sort,
-            currentPage: page,
-            totalPages,
-            totalRecords,
-            wishlist: UserCollectionCards,
+          const cardIDs = UserCollectionCards.map(card => card.card_ID);
+          
+
+          // Query to fetch paginated data with limit and offset
+          let cardsSQL = `SELECT * FROM cards WHERE name LIKE ? ORDER BY ${sort} LIMIT ? OFFSET ?`;
+          console.log(cardsSQL);
+          connection.query(cardsSQL, [`%${query}%`, limit, offset], (err, result) => {
+            if (err) {
+              console.error(err);
+              return res.status(500).send('Internal Server Error');
+            }
+
+            console.log('Better cards in collection: ', cardIDs);
+
+            // Render your page with the paginated data and total pages
+            res.render('cards/cards', {
+              user: req.session.user,
+              displayName: req.session.displayName,
+              cardsList: result,
+              limit,
+              currentQuery: query,
+              currentSort: sort,
+              currentPage: page,
+              totalPages,
+              totalRecords,
+              userCollection: cardIDs,
+            });
           });
         });
       });
-    });
+
+    } else {
+      // Query to fetch paginated data with limit and offset
+      let cardsSQL = `SELECT * FROM cards WHERE name LIKE ? ORDER BY ${sort} LIMIT ? OFFSET ?`;
+      console.log(cardsSQL);
+      connection.query(cardsSQL, [`%${query}%`, limit, offset], (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('Internal Server Error');
+        }
+
+        console.log(cardIDs);
+
+        // Render your page with the paginated data and total pages
+        res.render('cards/cards', {
+          user: req.session.user,
+          displayName: req.session.displayName,
+          cardsList: result,
+          limit,
+          currentQuery: query,
+          currentSort: sort,
+          currentPage: page,
+          totalPages,
+          totalRecords,
+          userCollection: cardIDs,
+        });
+      });
+    }
   });
 });
 
@@ -112,7 +150,6 @@ router.post("/add-to-collection", async (req, res) => {
 
   // Check if user is logged in
   if (!userId) {
-    console.log("User not logged in?");
     return res.status(401).json({ error: 'User not logged in' });
   }
 
@@ -135,7 +172,6 @@ router.post("/add-to-collection", async (req, res) => {
 
       // Now that we have the user's collection ID, perform the insert into collections_cards
       const insertQuery = 'INSERT INTO collections_cards (collection_ID, card_ID) VALUES (?, ?)';
-      console.log('Card ID again: ', cardId)
       connection.query(insertQuery, [userCollectionId, cardId], (insertErr, result) => {
         if (insertErr) {
           console.error('Error adding card to collection:', insertErr);
@@ -143,6 +179,65 @@ router.post("/add-to-collection", async (req, res) => {
         }
         console.log('Card added ');
         return res.status(200).json({ success: true });
+      });
+    });
+  } catch (error) {
+    console.error('Error adding card to collection:', error);
+    return res.status(500).json({ error: 'Error adding card to collection' });
+  }
+});
+
+// Remove From Collection
+router.post("/remove-from-collection", async (req, res) => {
+
+  const { cardId } = req.body; // Extract cardId from request body
+  const userId = req.session.user;
+
+  console.log('Card ID: ', cardId);
+  console.log('User ID: ', userId);
+
+  // Check if user is logged in
+  if (!userId) {
+    console.log("User not logged in?");
+    return res.status(401).json({ error: 'User not logged in' });
+  }
+
+  try {
+    // Fetch the user's collection ID from the database
+    const userCollectionIdQuery = `SELECT id FROM collection WHERE user_id = ? AND collection_type_ID = 1`;
+    connection.query(userCollectionIdQuery, [userId], (err, userCollectionIdResult) => {
+      if (err) {
+        console.error('Error fetching user collection ID:', err);
+        return res.status(500).json({ error: 'Error fetching user collection ID' });
+      }
+      // Check if the user's collection ID exists
+      if (userCollectionIdResult.length === 0) {
+        console.log("User collection ID not found");
+        return res.status(404).json({ error: 'User collection ID not found' });
+      }
+
+      const userCollectionId = userCollectionIdResult[0].id;
+
+      // Fetch the cards's card_collection ID
+      const collectionCardSQL = 'SELECT id FROM collections_cards WHERE collection_ID = ? AND card_ID = ?';
+      connection.query(collectionCardSQL, [userCollectionId, cardId], (err, collectionCardIdres) => {
+        if (err) {
+          console.error('Error fetching user collection ID:', err);
+          return res.status(500).json({ error: 'Error fetching user collection ID' });
+        }
+
+        const collectionCardId = collectionCardIdres[0].id;
+
+        // Remove card from card_collection table
+        const deleteQuery = 'DELETE FROM collections_cards WHERE id = ?';
+        connection.query(deleteQuery, [collectionCardId], (deleteErr, result) => {
+          if (deleteErr) {
+            console.error('Error removing card from collection:', deleteErr);
+            return res.status(500).json({ error: 'Error adding card to collection' });
+          }
+          console.log('Card removed.');
+          return res.status(200).json({ success: true });
+        });
       });
     });
   } catch (error) {
